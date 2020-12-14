8acc368a93b3e0b123391a16032fe04c3dfa5358
chengzhanghu <simple2012hcz@126.com>
2020-12-14 09:49:31 +0800

    deploy

---
+++ assets/js/10.5e84f57b.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{359:function(t,s,n){"use strict";n.r(s);var e={data:function(){return{msg:"Hello this is post component"}}},i=n(42),o=Object(i.a)(e,(function(){var t=this.$createElement;return(this._self._c||t)("p",{staticClass:"demo"},[this._v("\n  "+this._s(this.msg)+"\n")])}),[],!1,null,null,null);s.default=o.exports}}]);
